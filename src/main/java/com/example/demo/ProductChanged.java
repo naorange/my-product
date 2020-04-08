e39037ff63a16b555a4c924a1fe0ed0cd0964024
naorange <orange@sk.com>
2020-04-08 13:35:55 +0900

    4

---
+++ src/main/java/com/example/demo/ProductChanged.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.PostPersist;
+
+public class ProductChanged {
+    String eventType;
+    Long productId;
+    String productName;
+    int productStock;
+
+    public ProductChanged(){
+        this.eventType = this.getClass().getSimpleName();
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(int productStock) {
+        this.productStock = productStock;
+    }
+
+
+}
